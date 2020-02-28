279cac43af396664923d06b4e8cb9e963a333f77
Hira Khan <sunshine9.r8@gmail.com>
2020-02-28 12:16:28 -0500

    listbuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,17 +4,47 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = None            # the marker/pointer
         self.storage = DoublyLinkedList()
 
+        # adds items to the buffer
     def append(self, item):
-        pass
+        # let's define the base cases
+        # if there are less items then the capacity, we can simply add an item
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        # however, if the storage is full to the capacity
+        elif len(self.storage) == self.capacity:
+            # remove the oldest entry i.e. head
+            oldest = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            # reset the position again:
+            if oldest == self.current:
+                self.current = self.storage.tail
 
+
+        # returns the elements in order, does not return any None values.
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        # let's get a starting point
+        first = self.current
+        # we append that value
+        list_buffer_contents.append(first.value)
+        if first.next:
+            next_one = first.next
+        else:
+            next_one = self.storage.head
+        while next_one != first:
+            list_buffer_contents.append(next_one.value)
+            if next_one.next:
+                next_one = next_one.next
+            else:
+                next_one = self.storage.head
+        return list_buffer_contents
 
-        # TODO: Your code here
 
         return list_buffer_contents
 
